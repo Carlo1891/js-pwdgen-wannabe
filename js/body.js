71b403121ad4d2649b26976c7481ce06f94dd2e9
Carlo1891 <carlo.18@live.it>
2022-06-13 16:03:55 +0200

    Inizio

---
+++ js/body.js
@@ -0,0 +1,6 @@
+/** Chiediamo all’utente il suo nome,
+poi chiediamo il suo cognome,
+poi chiediamo il suo colore preferito.
+Infine scrivi sulla pagina nomecognomecolorepreferito22
+(sostituendo nome, cognome e colore preferito con quelli inseriti dall'utente)
+*/
