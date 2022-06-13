09767667c47ea49be14521da9ad2bd37a4a4199e
Carlo1891 <carlo.18@live.it>
2022-06-13 17:35:26 +0200

    Inserimento input utente

--- js/body.js
+++ js/body.js
@@ -4,3 +4,17 @@ poi chiediamo il suo colore preferito.
 Infine scrivi sulla pagina nomecognomecolorepreferito22
 (sostituendo nome, cognome e colore preferito con quelli inseriti dall'utente)
 */
+
+const userName = prompt ('Inserisci il tuo nome');
+
+const userLastName = prompt ('Inserisci il tuo cognome');
+
+const favouriteColor = prompt ('Inserisci il tuo colore preferito');
+
+console.log (userName, userLastName, favouriteColor);
+
+document.getElementById ('user-name').innerHTML = userName;
+
+document.getElementById ('user-lastname').innerHTML = userName;
+
+document.getElementById ('user-favcolor').innerHTML = userName;
